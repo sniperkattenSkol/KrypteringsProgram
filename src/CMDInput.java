d010f070c9bad8b7cd29a1cabcdb5991270fe906
sniperkattenskol <te15.nils.lindfors@umea.nti.se>
2017-08-30 13:47:01 +0200

    Initial commit

---
+++ src/CMDInput.java
@@ -0,0 +1,20 @@
+// Nils E Lindfors TE15 
+// 2017-08-30
+// 13:26
+public class CMDInput {
+
+    public void cmdArg(String [] argu){
+        if (argu.length > 0) {
+            
+        }
+
+    }
+
+    public static void main(String[] args) {
+
+    }
+
+
+}
+
+
